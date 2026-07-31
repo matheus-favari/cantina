edfb653852c1b21f18f28c87ccbb5884a31d7552
Matheus Favari <matheus.f.favari@gmail.com>
2026-07-31 18:57:27 -0300

    feat: add zod middleware and zod validation

--- backend/src/modules/auth/auth.routes.ts
+++ backend/src/modules/auth/auth.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import registerController from "./auth.controller";
+import { validate } from "../../middlewares/validate";
+import { registerSchema } from "./auth.schema";
 
 const routes = Router();
 
-routes.post("/register", registerController);
+routes.post("/register", validate(registerSchema), registerController);
 
 export default routes;
